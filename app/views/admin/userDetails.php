ecf0ce809fdcdfb03dfeb43d6b4a0122b4aa66ae
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2021-09-15 15:03:18 +0530

    add user profile UI

--- app/views/admin/userDetails.php
+++ app/views/admin/userDetails.php
@@ -9,70 +9,17 @@
     <title>ALec</title>
 
     <!-- CSS File HOME-->
-<!--    <link rel="stylesheet" href="/public/css/leaderboard.css">-->
-
-    <link rel="stylesheet" href="/public/css/user_details.css">
+    <?php linkCSS("user_details"); ?>
 
     <!-- Shortcut Icon -->
-    <link rel="shortcut icon" href="/public/img/logo1.png">
+    <?php shortIcon("logo1.png"); ?>
 
 </head>
 
 <body>
 
-    <div class="container">
+            <?php linkPhp("navigationBarAdmin"); ?>
 
-        <!-- NAVIGATION BAR - START -->
-        <div class="sidebar-container">
-            <div class="siderbar ">
-                <div class="logo_content">
-                    <div class="logo">
-                        <div class="logo_name">ALec</div>
-                    </div>
-                    <i class="fas fa-bars" id="btn"></i>
-                </div>
-                <ul class="nav_list">
-                    <li>
-                        <a href="#">
-                            <i class="fas fa-tachometer-alt"></i>
-                            <span class="links_name">Dashboard</span>
-                        </a>
-                        <span class="tooltip">Dashboard</span>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fas fa-user"></i>
-                            <span class="links_name">Create User</span>
-                        </a>
-                        <span class="tooltip">Create User</span>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fa fa-graduation-cap"></i>
-                            <span class="links_name">Create Courses</span>
-                        </a>
-                        <span class="tooltip">Create Course</span>
-                    </li>
-
-
-                    <div class="profile_content">
-                        <div class="profile">
-                            <div class="profile_details">
-                                <img alt="">
-                                <div class="name_job">
-                                    <div class="name">Admin</div>
-                                    <div class="job">Admin</div>
-                                </div>
-                            </div>
-                            <i class="fas fa-sign-out-alt" id="log_out" ></i>
-                        </div>
-                    </div>
-                </ul>
-            </div>
-        </div>
-        <!-- NAVIGATION BAR - END -->
-
-        <div class="home-content">
             <div class="details-container">
                 <header>User Details</header>
 
@@ -90,11 +37,6 @@
                     <button id="all" class="type active-type">All users</button>
                     <button id="lec" class="type">Lecturers</button>
                     <button id="stu" class="type">Students</button>
-<!--                    <button type="button">-->
-<!--                        <span>All users</span>-->
-<!--                        <span>Lecturers</span>-->
-<!--                        <span>Students</span>-->
-<!--                    </button>-->
                 </form>
             </div>
 
@@ -115,7 +57,7 @@
                         <td>1</td>
                         <td>Charith</td>
                         <td>Bandara</td>
-                        <td>stu</td>
+                        <td>Student</td>
                         <td>
                             <button type="button" class="button">
                                 <span class="button__text">View user</span>
@@ -127,7 +69,7 @@
                         <td>2</td>
                         <td>Janitha</td>
                         <td>Ratnayake</td>
-                        <td>stu</td>
+                        <td>Student</td>
                         <td>
                             <button type="button" class="button">
                                 <span class="button__text">View user</span>
@@ -139,9 +81,9 @@
                         <td>3</td>
                         <td>Pansilu</td>
                         <td>Gunaratna</td>
-                        <td>Lec</td>
+                        <td>Lecturer</td>
                         <td>
-                            <button type="button" class="button">
+                            <button type="button" class="button" onclick="window.location='<?php echo BASEURL . '/profileDetails/index'; ?> ">
                                 <span class="button__text">View user</span>
                             </button>
                         </td>
@@ -163,31 +105,8 @@
                 </table>
             </div>
 
-            <!-- FOOTER - START -->
+            <?php linkPhp("footer"); ?>
 
-            <footer class="sticky-footer">
-                <p> © Copyright ALec 2021.</p>
-            </footer>
-
-            <!-- FOOTER - END -->
-
-        </div>
-
-    </div>
-
-    <div class="pop-up">
-
-    </div>
-
-    <div class="notification-container" onclick="showNotifycation()">
-        <i class="fas fa-bell bell-notification"></i>
-    </div>
-
-    <!-- FontAwesome Icon -->
-    <script src="https://kit.fontawesome.com/3220c9480a.js" crossorigin="anonymous"></script>
-
-    <!-- **********JavaScript START********** -->
-    <script src="/public/js/basic.js"></script>
-    <!-- **********JavaScript END********** -->
+            <?php linkPhp("notification"); ?>
 
 </body>
