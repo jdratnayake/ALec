371eacb1a8a17333046b19f0a283c8a2150904c6
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2021-09-23 13:54:36 +0530

    Design changes on create quiz

--- app/views/lecturer/createQuiz.php
+++ app/views/lecturer/createQuiz.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Shortcut Icon -->
     <?php shortIcon("logo1.png"); ?>
@@ -24,13 +24,58 @@
     </header>
 
     <!-- Quiz basic details -->
-    <form class="details" method="post" action='<?php echo BASEURL . "/temp/submit";  ?>'>
-        <label for="quiz-name">Quiz name : </label>
-        <input type="text" class="text" placeholder="Enter quiz name" name="quiz-name"><br>
-        <label for="quiz-dur">Quiz duration : </label>
-        <input type="number" class="time" name="quiz-dur-hr" min="0" max="60"> hrs
-        <input type="number" class="time" name="quiz-dur-min" min="0" max="60"> mins
-        <input type="number" class="time" name="quiz-dur-sec" min="0" max="60"> secs
+    <form class="details" method="post" action='<?php echo BASEURL . "/temp/submit"; ?>'>
+        <div>
+            <!-- select course drop down list -->
+            <div class="form-group details">
+                <label for="course">Course</label>
+                <select name="course" id="course" class="form-control">
+                    <option value="def" selected>Select your course</option>
+                    <option value="one">SCS 1201 - Data Structures and Algorithms - I</option>
+                    <option value="two">SCS 1208 - Data Structures and Algorithms - II</option>
+                    <option value="three">SCS 2201 - Data Structures and Algorithms - III</option>
+                </select>
+            </div>
+
+            <div class="form-group details">
+                <!--        select topic drop down list -->
+                <label for="topic"> Topic:</label>
+                <select name="topic" id="topic" class="form-control">
+                    <option value="def" selected>Select your topic</option>
+                    <option value="1">Topic - 01</option>
+                    <option value="2">Topic - 02</option>
+                    <option value="3">Topic - 03</option>
+                    <option value="4">Topic - 04</option>
+                </select>
+            </div>
+
+            <!-- quiz details-->
+            <div class="form-group">
+                <label for="quiz-name">Quiz name</label>
+                <input type="text" class="form-control form-name" placeholder="Enter quiz name" name="quiz-name" id="quiz-name">
+            </div>
+
+            <div class="row">
+                <div class="form-group col">
+                    <label>Quiz Duration</label>
+                </div>
+
+                <div class="form-group col">
+                    <input type="number" class="form-control" placeholder="hrs" name="quiz-dur-hr" id="quiz-dur-hr" min="0" max="60">
+                    <label for="quiz-dur-hr"></label>
+                </div>
+
+                <div class="form-group col">
+                    <input type="number" class="form-control" placeholder="mins" name="quiz-dur-min" id="quiz-dur-min" min="0" max="60">
+                    <label for="quiz-dur-min"></label>
+                </div>
+
+                <div class="form-group col col-end">
+                    <input type="number" class="form-control" placeholder="secs" name="quiz-dur-sec" id="quiz-dur-sec" min="0" max="60">
+                    <label for="quiz-dur-sec"></label>
+                </div>
+            </div>
+        </div>
 
         <div id="form">
             <!-- This is the JS Created Element Look create_quiz.js -->
--- public/js/create_quiz.js
+++ public/js/create_quiz.js
@@ -9,6 +9,7 @@ class UI {
             <div class="form-header">
                 <h4>Question ${i}</h4>
                 <input type="hidden" name="type" value="${i}">
+                
                 <!-- Select question type -->
                 <div class="radio-inline" id=${i}>
                     <input type="radio" value="value_mcq" name="q${i}_type" checked class="mcq">
@@ -18,18 +19,21 @@ class UI {
                 </div>
             </div>
 
-            <div class="question-content " id="mcq_${i}">
-                <input type="text" class="form-question" placeholder="Enter your question here" name="q${i}_mcq">
+<!--            Question type - MCQ-->
+            <div class="question-content" id="mcq_${i}">
+                <div class="form-group">
+                    <textarea class="form-control" rows="4" name="q${i}_mcq" placeholder="Enter your question here"></textarea>
+                </div>
 
                 <!-- Answers list - START -->
-                <div class="answer">
+                <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer1">
                     <select id="q${i}_answer1_point" class="points" name="q1_answer1_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
                         <option value="25"> 25% </option>
-                        <option value=" 0"> 0% </option>
+                        <option value=" 0" selected="selected"> 0% </option>
                         <option value="-25"> -25% </option>
                         <option value="-50"> -50% </option>
                         <option value="-75"> -75% </option>
@@ -38,14 +42,14 @@ class UI {
                     <label for="q${i}_answer1_point">points</label>
                 </div>
 
-                <div class="answer">
+                <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer2">
                     <select id="q${i}_answer2_point" class="points" name="q1_answer2_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
                         <option value="25"> 25% </option>
-                        <option value=" 0"> 0% </option>
+                        <option value=" 0" selected="selected"> 0% </option>
                         <option value="-25"> -25% </option>
                         <option value="-50"> -50% </option>
                         <option value="-75"> -75% </option>
@@ -54,14 +58,14 @@ class UI {
                     <label for="q${i}_answer2_point">points</label>
                 </div>
 
-                <div class="answer">
+                <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer3">
                     <select id="q${i}_answer3_point" class="points" name="q1_answer3_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
                         <option value="25"> 25% </option>
-                        <option value=" 0"> 0% </option>
+                        <option value=" 0" selected="selected"> 0% </option>
                         <option value="-25"> -25% </option>
                         <option value="-50"> -50% </option>
                         <option value="-75"> -75% </option>
@@ -70,14 +74,14 @@ class UI {
                     <label for="q${i}_answer3_point">points</label>
                 </div>
 
-                <div class="answer">
+                <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer4">
                     <select id="q${i}_answer4_point" class="points" name="q1_answer4_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
                         <option value="25"> 25% </option>
-                        <option value=" 0"> 0% </option>
+                        <option value=" 0" selected="selected"> 0% </option>
                         <option value="-25"> -25% </option>
                         <option value="-50"> -50% </option>
                         <option value="-75"> -75% </option>
@@ -86,14 +90,14 @@ class UI {
                     <label for="q${i}_answer4_point">points</label>
                 </div>
 
-                <div class="answer">
+                <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer5">
                     <select id="q${i}_answer5_point" class="points" name="q1_answer5_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
                         <option value="25"> 25% </option>
-                        <option value=" 0"> 0% </option>
+                        <option value=" 0" selected="selected"> 0% </option>
                         <option value="-25"> -25% </option>
                         <option value="-50"> -50% </option>
                         <option value="-75"> -75% </option>
@@ -104,16 +108,19 @@ class UI {
                 <!-- Answers list - END -->
             </div>
 
+<!--            Question type - Short answer-->
             <div class="question-content short-ans" id="shortans_${i}">
-                <input type="text" class="form-question" placeholder="Enter your question here" name="q${i}_short">
-                <div class="answer">
+                <div class="form-group">
+                    <textarea class="form-control" placeholder="Enter your question here" name="q${i}_short" rows="5"></textarea>
+                </div>
+                <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter answer here" name="q${i}_shortanswer">
                     <select id="q${i}_shortanswer_point" class="points" name="q${i}_shortanswer_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
                         <option value="25"> 25% </option>
-                        <option value=" 0"> 0% </option>
+                        <option value=" 0" selected="selected"> 0% </option>
                         <option value="-25"> -25% </option>
                         <option value="-50"> -50% </option>
                         <option value="-75"> -75% </option>
