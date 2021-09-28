5b2b707dfd674b9ddd97f29388fef5cde0f40c22
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2021-09-29 01:37:05 +0530

    fix the bug on create quiz js

--- public/js/create_quiz.js
+++ public/js/create_quiz.js
@@ -1,10 +1,4 @@
 class UI {
-
-    // static close(i){
-    //     const question = document.getElementById("question_"+i);
-    //     question.style.display = "none";
-    // }
-
     static addQuestion(i) {
         const list = document.querySelector('form #form');
         const row = document.createElement('div');
@@ -14,8 +8,8 @@ class UI {
         row.innerHTML = `
             <div class="form-header">
             
-<!--                close button-->
-            <button class="close-btn" onclick="close()"><i class="fa fa-close"></i></button>
+<!--            close button-->
+            <button class="close-btn"><i class="fa fa-close"></i></button>
                 
                 <h4>Question ${i}</h4>
                 <input type="hidden" name="type" value="${i}">
@@ -38,7 +32,7 @@ class UI {
                 <!-- Answers list - START -->
                 <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer1">
-                    <select id="q${i}_answer1_point" class="points" name="q1_answer1_point">
+                    <select id="q${i}_answer1_point" class="points" name="q${i}_answer1_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
@@ -54,7 +48,7 @@ class UI {
 
                 <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer2">
-                    <select id="q${i}_answer2_point" class="points" name="q1_answer2_point">
+                    <select id="q${i}_answer2_point" class="points" name="q${i}_answer2_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
@@ -70,7 +64,7 @@ class UI {
 
                 <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer3">
-                    <select id="q${i}_answer3_point" class="points" name="q1_answer3_point">
+                    <select id="q${i}_answer3_point" class="points" name="q${i}_answer3_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
@@ -86,7 +80,7 @@ class UI {
 
                 <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer4">
-                    <select id="q${i}_answer4_point" class="points" name="q1_answer4_point">
+                    <select id="q${i}_answer4_point" class="points" name="q${i}_answer4_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
@@ -102,7 +96,7 @@ class UI {
 
                 <div class="form-group">
                     <input type="text" class="form-answer" placeholder="Enter your answer here" name="q${i}_answer5">
-                    <select id="q${i}_answer5_point" class="points" name="q1_answer5_point">
+                    <select id="q${i}_answer5_point" class="points" name="q${i}_answer5_point">
                         <option value="100"> 100% </option>
                         <option value="75"> 75% </option>
                         <option value="50"> 50% </option>
