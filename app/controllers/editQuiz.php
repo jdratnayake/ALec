3742db0db8ac9e418c89b57412c0359d00f4d68f
Janitha Ratnayake <janithadevin@gmail.com>
2021-10-14 01:34:51 +0530

    edit quiz backend halfy completed

--- app/controllers/editQuiz.php
+++ app/controllers/editQuiz.php
@@ -6,16 +6,47 @@ class EditQuiz extends AlecFramework
     {
         $this->authorization("lec");
         $this->helper("linker");
+        $this->editQuizModel = $this->model("editQuizModel");
     }
 
     public function index($quizId)
     {
         $data["quizId"] = $quizId;
+        $data["quizQuestionSummary"] = $this->editQuizModel->getQuizQuestionsSummary($quizId);
+        $data["quizQuestionChoices"] = $this->editQuizModel->getQuizQuestionChoices($quizId);
+
         $this->view("lecturer/editQuizView", $data);
     }
 
     public function submit($quizId)
     {
-        var_dump($_POST);
+        // var_dump($_POST);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $quizQuestionSummary = $this->editQuizModel->getQuizQuestionsSummary($quizId);
+            $quizQuestionChoices = $this->editQuizModel->getQuizQuestionChoices($quizId);
+
+            while ($row = mysqli_fetch_assoc($quizQuestionSummary)) {
+                $question = $_POST["q_$row[question_no]"];
+                $type = $type = "mcq-s";
+
+                if (isset($_POST["check_$row[question_no]"])) {
+                    $type = $type = "mcq-m";
+                }
+
+                $this->editQuizModel->updateQuestion($row["question_no"], $question, $type);
+
+                $count = $row["count"];
+
+                for ($i = 1; $i <= $count; $i++) {
+                    $choiceRow = mysqli_fetch_assoc($quizQuestionChoices);
+                    $choice = $_POST["choice_$choiceRow[choice_id]"];
+                    $points = $_POST["points_$choiceRow[choice_id]"];
+
+                    $this->editQuizModel->updateChoice($choiceRow["choice_id"], $choice, $points);
+                }
+            }
+        }
+
+        $this->index($quizId);
     }
 }
--- app/models/editQuizModel.php
+++ app/models/editQuizModel.php
@@ -4,7 +4,7 @@ class EditQuizModel extends Database
 {
     public function getQuizQuestionsSummary($quizId)
     {
-        $query = "SELECT quiz_question.question_no, question, COUNT(choice_id) AS count FROM quiz_question INNER JOIN question_choice ON quiz_question.question_no=question_choice.question_no WHERE quiz_question.quiz_id='$quizId' GROUP BY quiz_question.question_no ORDER BY quiz_question.question_no";
+        $query = "SELECT quiz_question.question_no, question, question_type, COUNT(choice_id) AS count FROM quiz_question INNER JOIN question_choice ON quiz_question.question_no=question_choice.question_no WHERE quiz_question.quiz_id='$quizId' GROUP BY quiz_question.question_no ORDER BY quiz_question.question_no";
 
         $result = mysqli_query($GLOBALS["db"], $query);
         return $result;
@@ -16,5 +16,23 @@ class EditQuizModel extends Database
 
         $result = mysqli_query($GLOBALS["db"], $query);
         return $result;
+    }
+
+    public function updateQuestion($questionId, $question, $questionType)
+    {
+        $query = "UPDATE quiz_question
+        SET question='$question', question_type='$questionType'
+        WHERE question_no='$questionId'";
+
+        mysqli_query($GLOBALS["db"], $query);
+    }
+
+    public function updateChoice($choiceId, $choice, $points)
+    {
+        $query = "UPDATE question_choice
+        SET choice_name='$choice', points='$points'
+        WHERE choice_id='$choiceId'";
+
+        mysqli_query($GLOBALS["db"], $query);
     }
 }
