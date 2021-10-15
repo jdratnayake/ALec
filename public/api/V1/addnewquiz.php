59b31972a0425593bd995acedf1945755846ac8d
CharithAnjana <charithanjana01@gmail.com>
2021-10-15 12:22:39 +0530

    add create quiz api and push

--- public/api/V1/addnewquiz.php
+++ public/api/V1/addnewquiz.php
@@ -5,10 +5,9 @@
 	$topic_id = $_POST["topic_ID"];
 	$lecturer_id = $_POST["lecturer_ID"];
 	$quiz_name = $_POST["quiz_Name"];
-	$dur_hr = $_POST["du_Hr"];
-	$dur_min = $_POST["du_Min"];
+	$dur = $_POST["dur"];
 	$status = "draft";
-	$dur = "{$dur_hr}:{$dur_min}:00";
+	//$dur = "{$dur_hr}:{$dur_min}:00";
 	
 	$sql = "INSERT INTO quiz(quiz_id, quiz_name, status, create_date, published_date, close_date, duration, lecturer_id, topic_id) 
 			  VALUES (NULL, '$quiz_name', '$status', NOW(), NULL, NULL, '$dur', '$lecturer_id', '$topic_id');";
@@ -18,8 +17,9 @@
 		$sqls = "SELECT quiz_id FROM quiz ORDER BY quiz_id DESC LIMIT 1";
 		$results = mysqli_query($conn, $sqls);
 		
-		if($result){
-			print(mysqli_fetch_assoc($result)["quiz_id"]);
+		if($results){
+			$row = mysqli_fetch_assoc($results);
+			echo $row['quiz_id'];
 		}
 	}
 	else{
