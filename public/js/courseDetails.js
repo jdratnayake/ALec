e07d836b8bd9a60a9497b3fbb8eba9855dd4a847
Janitha Ratnayake <janithadevin@gmail.com>
2021-10-17 23:16:09 +0530

    courseDetails drop down bug resolved

--- public/js/courseDetails.js
+++ public/js/courseDetails.js
@@ -31,15 +31,8 @@ $(document).ready(function () {
 
     // LIVE SEARCH END
 
-
-    $("#all").click(function () {
-        $("#all").addClass("active-type");
-        $("#one").removeClass("active-type");
-        $("#two").removeClass("active-type");
-        $("#three").removeClass("active-type");
-        $("#four").removeClass("active-type");
-
-        const temp = "all";
+    $("#year").change(function () {
+        const temp = $(this).val();
 
         $.ajax({
             type: "GET",
@@ -55,95 +48,4 @@ $(document).ready(function () {
         })
     });
 
-    $("#one").click(function () {
-        $("#one").addClass("active-type");
-        $("#all").removeClass("active-type");
-        $("#two").removeClass("active-type");
-        $("#three").removeClass("active-type");
-        $("#four").removeClass("active-type");
-
-        const temp = "1";
-
-        $.ajax({
-            type: "GET",
-
-            url: "http://localhost/ALec/courseDetails/ajaxCourseDetails/" + temp,
-            dataType: "html",
-            // data: {
-            //     courseId: temp
-            // },
-            success: function (response) {
-                $("#table-content").html(response);
-            }
-        })
-    });
-
-    $("#two").click(function () {
-        $("#two").addClass("active-type");
-        $("#all").removeClass("active-type");
-        $("#one").removeClass("active-type");
-        $("#three").removeClass("active-type");
-        $("#four").removeClass("active-type");
-
-        const temp = "2";
-
-        $.ajax({
-            type: "GET",
-
-            url: "http://localhost/ALec/courseDetails/ajaxCourseDetails/" + temp,
-            dataType: "html",
-            // data: {
-            //     courseId: temp
-            // },
-            success: function (response) {
-                $("#table-content").html(response);
-            }
-        })
-    });
-
-    $("#three").click(function () {
-        $("#three").addClass("active-type");
-        $("#all").removeClass("active-type");
-        $("#one").removeClass("active-type");
-        $("#two").removeClass("active-type");
-        $("#four").removeClass("active-type");
-
-        const temp = "3";
-
-        $.ajax({
-            type: "GET",
-
-            url: "http://localhost/ALec/courseDetails/ajaxCourseDetails/" + temp,
-            dataType: "html",
-            // data: {
-            //     courseId: temp
-            // },
-            success: function (response) {
-                $("#table-content").html(response);
-            }
-        })
-    });
-
-    $("#four").click(function () {
-        $("#four").addClass("active-type");
-        $("#all").removeClass("active-type");
-        $("#one").removeClass("active-type");
-        $("#three").removeClass("active-type");
-        $("#two").removeClass("active-type");
-
-        const temp = "4";
-
-        $.ajax({
-            type: "GET",
-
-            url: "http://localhost/ALec/courseDetails/ajaxCourseDetails/" + temp,
-            dataType: "html",
-            // data: {
-            //     courseId: temp
-            // },
-            success: function (response) {
-                $("#table-content").html(response);
-            }
-        })
-    });
 })
