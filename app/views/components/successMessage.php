d868fdfafeeeb76ebcf5428d7b7a09bf61a076ef
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2021-10-25 23:09:32 +0530

    Update css of success message

--- app/views/components/successMessage.php
+++ app/views/components/successMessage.php
@@ -2,12 +2,12 @@
 <div class="message-container" id="success-box">
     <!--        style="display: none"-->
     <div class="close-btn" id="close-btn"><i class="fa fa-times-circle-o" aria-hidden="true"></i></div>
-    <div class="face success"><i class="fa fa-check-circle" aria-hidden="true"></i></div>
-    <div class="message">
-        <h1 class="alert success">SUCCESS!</h1>
-        <p>yay, everything is working :)</p>
+    <div class="face success-msg"><i class="fa fa-check-circle" aria-hidden="true"></i></div>
+    <div class="message-content">
+        <h1 class="alert success-msg tag-txt">Success!</h1>
+        <p class="message-txt">yay, everything is working :)</p>
     </div>
-    <button class="button-box blue-btn" id="continue-button"><span>continue</span></button>
+    <button class="button-box blue-btn" id="continue-button"><span class="tag-txt">continue</span></button>
 </div>
 
 <?php linkJS("successMessage"); ?>
