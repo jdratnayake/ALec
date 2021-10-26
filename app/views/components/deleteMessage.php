a619c5a4e3ba1c0f2f5a3b6979070ab6f4d1b100
Janitha Ratnayake <janithadevin@gmail.com>
2021-10-26 14:13:46 +0530

    delete message styles and code added

---
+++ app/views/components/deleteMessage.php
@@ -0,0 +1,14 @@
+<!--Confirmation message box-->
+<div class="message-container" id="error-box">
+    <!--       style="display: none"-->
+    <!--    <div class="close-btn"><i class="fa fa-times-circle-o" aria-hidden="true"></i></div>-->
+    <div class="face error-msg"><i class="fa fa-exclamation-circle" aria-hidden="true"></i></div>
+    <div class="message-content">
+        <h1 class="alert error-msg tag-txt">Warning!</h1>
+        <p class="message-txt">Are you sure you want to continue?
+    </div>
+    <div class="button-boxes">
+        <button class="red-btn confirm-btn"><span class="tag-txt">Yes, delete</span></button>
+        <button class="red-btn confirm-btn cancel-btn"><span class="tag-txt">No, cancel</span></button>
+    </div>
+</div>
