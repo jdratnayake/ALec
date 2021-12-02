d1d09eebc82a31f007160965bfa13f06801a1626
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2021-12-02 11:54:43 +0530

    Update page title

--- app/views/lecturer/displaySessions.php
+++ app/views/lecturer/displaySessions.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Course Badges</title>
+    <title>Sessions</title>
 
     <!-- CSS File HOME-->
     <?php linkCSS("display_sessions"); ?>
