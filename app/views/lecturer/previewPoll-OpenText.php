3972cb83076519bddc3c536f855bf003172294ba
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2021-12-03 23:46:13 +0530

    Add preview-openText-polls view

---
+++ app/views/lecturer/previewPoll-OpenText.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Preview Poll</title>
+
+    <!-- CSS File HOME-->
+    <?php linkCSS("preview_Poll_OpenText"); ?>
+
+    <!-- Shortcut Icon -->
+    <?php shortIcon("logo1.jpg"); ?>
+
+</head>
+
+<body>
+<?php linkPhp("navigationBarLecturer"); ?>
+
+<!--    breadcrumb-->
+<ul class="breadcrumb">
+    <li><a href="http://localhost/ALec/adminDashboard/index">Home</a></li>
+    <li><a href="#">Sessions</a></li>
+    <li><a href="#">New Poll Question</a></li>
+    <li><a href="#">Create Open Text Question</a></li>
+    <li>Preview Poll</li>
+</ul>
+
+<div class="details-content">
+    <div class="container">
+        <div class="heading">
+            Open Text Poll Question
+        </div>
+        <div class="content">
+            <div class="time">
+                <div class="time-bar"></div>
+            </div>
+            <p id="time-display">40 secs remaining</p>
+            <!--            open text poll question-->
+            <label for="question"></label>
+            <input type="text" class="session" placeholder="Enter your question here..." id="question">
+        </div>
+
+        <div class="button-container">
+            <!--        Save Session Button-->
+            <button type="button" value="Create Session" class="save-btn">Save Question</button>
+        </div>
+    </div>
+</div>
+
+<?php linkPhp("footer"); ?>
+
+<?php linkPhp("notification"); ?>
+
+<?php linkJS("createPollMcq"); ?>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
