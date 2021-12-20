eb9b07f2afe124f9a164031513776d939a204831
Janitha Ratnayake <janithadevin@gmail.com>
2021-12-20 12:51:31 +0530

    createPoll mcq - completed

--- app/controllers/createPoll.php
+++ app/controllers/createPoll.php
@@ -26,6 +26,8 @@ class CreatePoll extends AlecFramework
         //Form submission - START
         $errors = array();
 
+        $errors["duration"] = "";
+
         //Mcq
         $errors["question"] = "";
         $errors["answer-1"] = "";
@@ -41,15 +43,19 @@ class CreatePoll extends AlecFramework
             // var_dump($_POST);
             // return 0;
             $questionType = $_POST["question-type"];
+            $duration = $_POST["quiz-dur"];;
+
             $data["questionType"] = $questionType;
+
+            if (empty($duration)) $errors["duration"] = "Duration is required";
 
             if ($questionType == "mcq") {
                 $question = $_POST["question"];
                 $answer1 = $_POST["answer-1"];
                 $answer2 = $_POST["answer-2"];
-                $answer3 = $_POST["answer-3"];
-                $answer4 = $_POST["answer-4"];
-                $answer5 = $_POST["answer-5"];
+                // $answer3 = $_POST["answer-3"];
+                // $answer4 = $_POST["answer-4"];
+                // $answer5 = $_POST["answer-5"];
 
                 //Empty check
                 if (empty($question)) $errors["question"] = "Question is required";
@@ -74,6 +80,30 @@ class CreatePoll extends AlecFramework
             }
 
             if ($numberOfErrors == 0) {
+                $quizDur = explode(":", $duration);
+
+                $durHr = $quizDur[0];
+                $durMin = $quizDur[1];
+                $durSec = $quizDur[2];
+
+                if ($questionType == "mcq") {
+                    $questionId = $this->createPollModel->addQuestion($sessionId, "mcq", $question, $durHr, $durMin, $durSec);
+                    $this->createPollModel->addAnswer($questionId, $sessionId, $answer1);
+                    $this->createPollModel->addAnswer($questionId, $sessionId, $answer2);
+
+                    for ($i = 3; $i <= 5; $i++) {
+                        if (!empty($_POST["answer-{$i}"])) {
+                            $answer = $_POST["answer-{$i}"];
+                            $this->createPollModel->addAnswer($questionId, $sessionId, $answer);
+                        } else {
+                            break;
+                        }
+                    }
+                } else if ($questionType == "open") {
+                    $questionId = $this->createPollModel->addQuestion($sessionId, "mcq-tf", $question, $durHr, $durMin, $durSec);
+                    $this->createPollModel->addAnswer($questionId, $sessionId, "TRUE");
+                    $this->createPollModel->addAnswer($questionId, $sessionId, "FALSE");
+                }
             }
         }
 
--- app/models/createPollModel.php
+++ app/models/createPollModel.php
@@ -9,4 +9,31 @@ class CreatePollModel extends Database
 
         return mysqli_fetch_assoc($result);
     }
+
+    public function addQuestion($sessionId, $type, $question, $durHr, $durMin, $durSec)
+    {
+        $type = mysqli_real_escape_string($GLOBALS["db"], $type);
+        $question = mysqli_real_escape_string($GLOBALS["db"], $question);
+        $durHr = mysqli_real_escape_string($GLOBALS["db"], $durHr);
+        $durMin = mysqli_real_escape_string($GLOBALS["db"], $durMin);
+        $durSec = mysqli_real_escape_string($GLOBALS["db"], $durSec);
+
+        $dur = "{$durHr}:{$durMin}:{$durSec}";
+
+        $query = "INSERT INTO session_question(session_id, question_type, question, duration) VALUES('$sessionId', '$type', '$question', '$dur')";
+        mysqli_query($GLOBALS["db"], $query);
+
+        $query = "SELECT question_no FROM session_question ORDER BY question_no DESC LIMIT 1";
+        $result = mysqli_query($GLOBALS["db"], $query);
+
+        return mysqli_fetch_assoc($result)["question_no"];
+    }
+
+    public function addAnswer($questionId, $sessionId, $choice)
+    {
+        $choice = mysqli_real_escape_string($GLOBALS["db"], $choice);
+
+        $query = "INSERT INTO session_answer(question_no, session_id, choice_name) VALUES('$questionId', '$sessionId', '$choice')";
+        mysqli_query($GLOBALS["db"], $query);
+    }
 }
