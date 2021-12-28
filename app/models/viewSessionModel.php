a2ced3248b38766769bb8f43857e98e64ffc36c5
Janitha Ratnayake <janithadevin@gmail.com>
2021-12-28 10:31:27 +0530

    auto disable session question

--- app/models/viewSessionModel.php
+++ app/models/viewSessionModel.php
@@ -25,10 +25,31 @@ class ViewSessionModel extends Database
 
             $query = "UPDATE session_question SET status='T', published_time=NOW() WHERE session_id='$sessionId' AND question_no='$questionId'";
             mysqli_query($GLOBALS["db"], $query);
+
+            //Set the schedule
+            $this->setSchedule($sessionId, $questionId);
         } else if ($status == "F") {
             $query = "UPDATE session_question SET status='F', published_time=NULL WHERE session_id='$sessionId' AND question_no='$questionId'";
             mysqli_query($GLOBALS["db"], $query);
         }
+    }
+
+    public function setSchedule($sessionId, $questionId)
+    {
+        //Get duration
+        $query = "SELECT TIME_TO_SEC(duration) AS duration FROM session_question WHERE question_no='$questionId'";
+        $result = mysqli_query($GLOBALS["db"], $query);
+        $duration = mysqli_fetch_assoc($result)["duration"];
+
+        //Set schedule
+        $query =
+            "
+        CREATE EVENT session_question_event_{$sessionId}
+        ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL $duration SECOND
+        DO
+        UPDATE session_question SET status='F' WHERE question_no='$questionId'
+        ";
+        mysqli_query($GLOBALS["db"], $query);
     }
 
     public function getSessionQuestions($sessionId)
