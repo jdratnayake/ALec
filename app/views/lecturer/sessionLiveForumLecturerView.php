37c43382d943a4eb25f7030ff4553f23bcadb4e6
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-03-17 10:30:40 +0530

    Add check resolved option to lecturer live forum

--- app/views/lecturer/sessionLiveForumLecturerView.php
+++ app/views/lecturer/sessionLiveForumLecturerView.php
@@ -22,41 +22,41 @@ $sessionName = $data["bread"]["sessionDetails"]["session_name"];
 </head>
 
 <body>
-    <input type="hidden" id="session-id" value='<?php echo $sessionId; ?>'>
-    <input type="hidden" id="questionIdArray" value='<?php echo $data["questionIdArray"]; ?>'>
+<input type="hidden" id="session-id" value='<?php echo $sessionId; ?>'>
+<input type="hidden" id="questionIdArray" value='<?php echo $data["questionIdArray"]; ?>'>
 
-    <?php linkPhp("navigationBarLecturer"); ?>
+<?php linkPhp("navigationBarLecturer"); ?>
 
-    <!--    breadcrumb-->
-    <ul class="breadcrumb">
-        <li><a href="http://localhost/ALec/lecturerDashboard">Home</a></li>
+<!--    breadcrumb-->
+<ul class="breadcrumb">
+    <li><a href="http://localhost/ALec/lecturerDashboard">Home</a></li>
 
-        <li><a href="http://localhost/ALec/displaySessionsList/index">Sessions</a></li>
+    <li><a href="http://localhost/ALec/displaySessionsList/index">Sessions</a></li>
 
-        <?php
-        echo
-        "
+    <?php
+    echo
+    "
         <li><a href='http://localhost/ALec/viewSession/index/{$sessionId}'>$sessionName</a></li>
         ";
-        ?>
+    ?>
 
-        <li>Live Forum</li>
-    </ul>
+    <li>Live Forum</li>
+</ul>
 
-    <div class="details-content">
+<div class="details-content">
 
-        <div class="header-container">
-            <header>Live Forum</header>
-        </div>
+    <div class="header-container">
+        <header>Live Forum</header>
+    </div>
 
-        <div class="questions-container">
+    <div class="questions-container">
 
-            <?php
+        <?php
 
-            while ($row = mysqli_fetch_assoc($data["questionDetails"])) {
+        while ($row = mysqli_fetch_assoc($data["questionDetails"])) {
 
-                echo
-                "
+            echo
+            "
                 <div class='question'>
                     <input type='hidden' value='{$row['question_id']}'>
                     <span class='text'>
@@ -65,28 +65,29 @@ $sessionName = $data["bread"]["sessionDetails"]["session_name"];
                         <span class='name'>{$row['post_time']}</span>
                     </span>
                     <span class='vote'>
-                        <i class='fa fa-thumbs-o-up vote-highlight' aria-hidden='true'></i>
-                        <span class='votes-count'>{$row['points']}</span>
+                        <!--<i class='fa fa-thumbs-o-up vote-highlight' aria-hidden='true'></i>
+                        <span class='votes-count'>{$row['points']}</span> -->
+                        <i class='fa fa-check-circle check-resolved' aria-hidden='true'></i>
                     </span>
                 </div>
                 ";
-            }
+        }
 
-            ?>
-
-        </div>
+        ?>
 
     </div>
 
-    <?php linkPhp("footer"); ?>
+</div>
 
-    <?php linkPhp("notificationView"); ?>
+<?php linkPhp("footer"); ?>
 
-    <?php linkJS("lib/jquery-3.6.0.min"); ?>
+<?php linkPhp("notificationView"); ?>
 
-    <?php linkJS("notification") ?>
+<?php linkJS("lib/jquery-3.6.0.min"); ?>
 
-    <?php linkJS("sessionLiveForumLecturerQuestionsUpdate") ?>
+<?php linkJS("notification") ?>
+
+<?php linkJS("sessionLiveForumLecturerQuestionsUpdate") ?>
 
 </body>
 
--- public/js/sessionLiveForumLecturerQuestionsUpdate.js
+++ public/js/sessionLiveForumLecturerQuestionsUpdate.js
@@ -36,3 +36,9 @@ $(document).ready(function () {
         })
     }
 })
+
+for (let i = 0; i < document.getElementsByClassName("check-resolved").length; i++) {
+    document.getElementsByClassName('check-resolved')[i].addEventListener("click", () => {
+        document.getElementsByClassName('check-resolved')[i].style.color = "#ccc";
+    })
+}
