bc91874a2c060ee85f5fd05a5a42f1c7a6bf2467
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-03-17 16:31:14 +0530

    Add resolved class to live forum

--- app/views/lecturer/sessionLiveForumLecturerView.php
+++ app/views/lecturer/sessionLiveForumLecturerView.php
@@ -22,52 +22,52 @@ $sessionName = $data["bread"]["sessionDetails"]["session_name"];
 </head>
 
 <body>
-    <input type="hidden" id="session-id" value='<?php echo $sessionId; ?>'>
-    <input type="hidden" id="questionIdArray" value='<?php echo $data["questionIdArray"]; ?>'>
+<input type="hidden" id="session-id" value='<?php echo $sessionId; ?>'>
+<input type="hidden" id="questionIdArray" value='<?php echo $data["questionIdArray"]; ?>'>
 
-    <?php linkPhp("navigationBarLecturer"); ?>
+<?php linkPhp("navigationBarLecturer"); ?>
 
-    <!--    breadcrumb-->
-    <ul class="breadcrumb">
-        <li><a href="http://localhost/ALec/lecturerDashboard">Home</a></li>
+<!--    breadcrumb-->
+<ul class="breadcrumb">
+    <li><a href="http://localhost/ALec/lecturerDashboard">Home</a></li>
 
-        <li><a href="http://localhost/ALec/displaySessionsList/index">Sessions</a></li>
+    <li><a href="http://localhost/ALec/displaySessionsList/index">Sessions</a></li>
 
-        <?php
-        echo
-        "
+    <?php
+    echo
+    "
         <li><a href='http://localhost/ALec/viewSession/index/{$sessionId}'>$sessionName</a></li>
         ";
-        ?>
+    ?>
 
-        <li>Live Forum</li>
-    </ul>
+    <li>Live Forum</li>
+</ul>
 
-    <div class="details-content">
+<div class="details-content">
 
-        <div class="header-container">
-            <header>Live Forum</header>
+    <div class="header-container">
+        <header>Live Forum</header>
+    </div>
+
+    <div class="questions-container">
+
+        <div class="stat-container">
+            <div class="resolved stat-box">
+                <span>Resolved</span>
+                <span><?php echo $data["resolvedCount"]; ?></span>
+            </div>
+            <div class="unresolved stat-box">
+                <span>Unresolved</span>
+                <span><?php echo $data["unResolvedCount"]; ?></span>
+            </div>
         </div>
 
-        <div class="questions-container">
+        <?php
 
-            <div class="stat-container">
-                <div class="resolved stat-box">
-                    <span>Resolved</span>
-                    <span><?php echo $data["resolvedCount"]; ?></span>
-                </div>
-                <div class="unresolved stat-box">
-                    <span>Unresolved</span>
-                    <span><?php echo $data["unResolvedCount"]; ?></span>
-                </div>
-            </div>
+        while ($row = mysqli_fetch_assoc($data["questionDetails"])) {
 
-            <?php
-
-            while ($row = mysqli_fetch_assoc($data["questionDetails"])) {
-
-                echo
-                "
+            echo
+            "
                 <div class='question'>
                     <input type='hidden' value='{$row['question_id']}'>
                     <span class='text'>
@@ -78,27 +78,27 @@ $sessionName = $data["bread"]["sessionDetails"]["session_name"];
                     <span class='vote'>
                         <!--<i class='fa fa-thumbs-o-up vote-highlight' aria-hidden='true'></i>
                         <span class='votes-count'>{$row['points']}</span> -->
-                        <i class='fa fa-check-circle check-resolved' aria-hidden='true'></i>
+                        <i class='fa fa-check-circle check-resolved resolved' aria-hidden='true'></i>
                     </span>
                 </div>
                 ";
-            }
+        }
 
-            ?>
-
-        </div>
+        ?>
 
     </div>
 
-    <?php linkPhp("footer"); ?>
+</div>
 
-    <?php linkPhp("notificationView"); ?>
+<?php linkPhp("footer"); ?>
 
-    <?php linkJS("lib/jquery-3.6.0.min"); ?>
+<?php linkPhp("notificationView"); ?>
 
-    <?php linkJS("notification") ?>
+<?php linkJS("lib/jquery-3.6.0.min"); ?>
 
-    <?php linkJS("sessionLiveForumLecturerQuestionsUpdate") ?>
+<?php linkJS("notification") ?>
+
+<?php linkJS("sessionLiveForumLecturerQuestionsUpdate") ?>
 
 </body>
 
