4846367ce54e40d9d3ff8b0162b29c05a6fe7334
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-03-19 01:13:22 +0530

    Add changes to display drafts views

--- app/views/lecturer/displayDraftsListView.php
+++ app/views/lecturer/displayDraftsListView.php
@@ -9,7 +9,7 @@
     <title>drafts</title>
 
     <!-- CSS File HOME-->
-    <?php linkCSS("display_draftquizzes"); ?>
+    <?php linkCSS("display_draft_quizzes"); ?>
 
     <!-- Shortcut Icon -->
     <?php shortIcon("logo1.jpg"); ?>
@@ -28,61 +28,53 @@
 <div class="details-content">
     <div class="header-container">
         <header>Drafts</header>
-    </div>
-    <div class="button-container">
-        <!--    Search bar     -->
-        <form method="POST" class="search-bar" id="draft-search">
-            <label for="search-tag"></label>
 
-            <input type="text" placeholder="Search.. &#xF002;" name="search-tag" id="search-tag"
-                   style="font-family: FontAwesome, Poppins,sans-serif; font-style: normal; font-size: 15px">
-        </form>
+<!--        Course selection drop down-->
+        <div class="course-selection">
+            <label for="course-selection"></label>
+            <select name="course-selection" id="course-selection" class="course-dropdown">
+                <option value='course-0'>--No course selected--</option>
+                <option value='course-1'>SCS 1201 - DATA STRUCTURES AND ALGORITHMS - I</option>
+                <option value='course-2'>SCS 1201 - DATA STRUCTURES AND ALGORITHMS - II</option>
+                <option value='course-3'>SCS 1201 - DATA STRUCTURES AND ALGORITHMS - III</option>
+            </select>
+        </div>
+
+<!--        Topic selection drop down-->
+        <div class="course-selection">
+            <label for="topic-selection"></label>
+            <select name="topic-selection" id="topic-selection" class="course-dropdown">
+                <option value='topic-0'>--No course selected--</option>
+                <option value='topic-1'>TOPIC - I</option>
+                <option value='topic-2'>TOPIC - II</option>
+                <option value='topic-3'>TOPIC - III</option>
+            </select>
+        </div>
+    </div>
+
+    <div class="button-container">
         <!--        Add draft Button-->
         <button type="button" value="Add New Quiz" class="add-btn" id="add-quiz-btn">Add New Quiz</button>
     </div>
 
-    <!--    Division of All Quizzes-->
     <div class="draft-details-container">
-        <!--    Division of Active drafts-->
         <div class="drafts">
-            <span class=" draft-label">Today</span>
-
-            <div id="active-drafts">
                 <div class='draft'>
                     <div class='draft-details'>
-                        <a href="" class='draft-name'>
-                                    <span>
-                                        Quiz 1
-                                    </span>
+                        <a href="" class='draft-name'><span>Quiz 1</span>
                         </a>
                     </div>
-                    <span class='draft-created-date' style='text-decoration: none; font-size: 10px'>
-                                    17/03/2022
-                            </span>
+                    <span class='draft-created-date'>17/03/2022</span>
                 </div>
-
-            </div>
         </div>
 
-        <!--    Division of other drafts-->
         <div class="drafts">
-            <span class="draft-label">Old</span>
-
-            <div id="inactive-drafts">
                 <div class='draft'>
                     <div class='draft-details'>
-                        <a href='' class='draft-name'>
-                                <span>
-                                    Quiz 2
-                                </span>
-                        </a>
+                        <a href='' class='draft-name'><span>Quiz 2</span></a>
                     </div>
-                    <span class='draft-created-date' style='text-decoration: none; font-size: 10px'>
-                                    12/01/2022
-                            </span>
+                    <span class='draft-created-date'>12/01/2022</span>
                 </div>
-            </div>
-
         </div>
     </div>
 
