1b5e2f7e10183f07dd256fa11c7f264a8125671d
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-03-20 23:31:33 +0530

    Add back button to display results

--- app/views/student/displayResultsView.php
+++ app/views/student/displayResultsView.php
@@ -15,75 +15,78 @@
 </head>
 
 <body>
-    <?php linkPhp("navigationBarStudent"); ?>
+<?php linkPhp("navigationBarStudent"); ?>
 
-    <div class="form-content">
-        <!-- Quiz heading -->
-        <header>Quiz 1</header>
+<div class="form-content">
+    <!-- Quiz heading -->
+    <header>Quiz 1</header>
 
-        <!--quizCompletedResult-->
-        <div class="quizCompleted has-text-centered" id="result">
+    <!--quizCompletedResult-->
+    <div class="quizCompleted has-text-centered" id="result">
 
-            <!-- quizCompletedIcon: Achievement Icon -->
-            <span class="icon">
+        <!-- quizCompletedIcon: Achievement Icon -->
+        <span class="icon">
                 <i class="fa fa-check-circle-o"></i>
             </span>
 
-            <h2 class="title">
-                Quiz Successfully Completed!
-            </h2>
-            <p class="subtitle">
-                Total score: 6/10
-            </p>
+        <h2 class="title">
+            Quiz Successfully Completed!
+        </h2>
+        <p class="subtitle">
+            Total score: 72%
+        </p>
 
-            <div class="scores">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Question</th>
-                            <th>Marks</th>
-                            <th>Success Rate</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Question 1</td>
-                            <td>2.5</td>
-                            <td>98.00%</td>
-                        </tr>
-                        <tr>
-                            <td>Question 2</td>
-                            <td>0</td>
-                            <td>1.20%</td>
-                        </tr>
-                        <tr>
-                            <td>Question 3</td>
-                            <td>2</td>
-                            <td>67.15%</td>
-                        </tr>
+        <div class="scores">
+            <table>
+                <thead>
+                <tr>
+                    <th>Question</th>
+                    <th>Marks</th>
+                    <th>Success Rate</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Question 1</td>
+                    <td>2.5</td>
+                    <td>98.00%</td>
+                </tr>
+                <tr>
+                    <td>Question 2</td>
+                    <td>0</td>
+                    <td>1.20%</td>
+                </tr>
+                <tr>
+                    <td>Question 3</td>
+                    <td>2</td>
+                    <td>67.15%</td>
+                </tr>
 
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td colspan="3"> Total Marks = 6</td>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
-
-
+                </tbody>
+                <tfoot>
+                <tr>
+                    <td colspan="3">Total score: 72%</td>
+                </tr>
+                </tfoot>
+            </table>
         </div>
-        <!--/quizCompetedResult-->
+
+        <div class="button-container">
+            <a href="">Back</a>
+        </div>
 
     </div>
+    <!--/quizCompetedResult-->
 
-    <?php linkPhp("notificationView"); ?>
+</div>
 
-    <?php linkPhp("footer"); ?>
+<?php linkPhp("notificationView"); ?>
 
-    <?php linkJS("lib/jquery-3.6.0.min"); ?>
+<?php linkPhp("footer"); ?>
 
-    <?php linkJS("notification") ?>
+<?php linkJS("lib/jquery-3.6.0.min"); ?>
+
+<?php linkJS("notification") ?>
 
 </body>
 
