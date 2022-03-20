3abe2bffc77bfc33c4332d2620043352a43b446d
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-03-20 18:52:11 +0530

    Update user edit profile view

--- app/views/editProfileView.php
+++ app/views/editProfileView.php
@@ -18,69 +18,75 @@
 
 <body>
 
-    <?php linkPhp("navigationBarLecturer"); ?>
+<?php linkPhp("navigationBarLecturer"); ?>
 
-    <!--    breadcrumb-->
-    <ul class="breadcrumb">
-        <li><a href="http://localhost/ALec/lecturerDashboard/index">Home</a></li>
-        <li>Edit profile</li>
-    </ul>
+<!--    breadcrumb-->
+<ul class="breadcrumb">
+    <li><a href="http://localhost/ALec/lecturerDashboard/index">Home</a></li>
+    <li>Edit profile</li>
+</ul>
 
-    <div class="details-content">
-        <div class="header-container">
-            <header>
-                User Profile
-            </header>
+<div class="details-content">
+
+    <div class="user-details-container">
+        <div class="header-bar">
+            <img <?php srcIMG("user_avatar.png"); ?> alt="User image">
+            <div class="bio">
+                <span><?php echo $data["userDetails"]["first_name"] . " " . $data["userDetails"]["last_name"]; ?></span>
+                <span><?php echo $data["userDetails"]["user_type"]; ?></span>
+                <span><?php echo $data["userDetails"]["reg_no"]; ?></span>
+            </div>
         </div>
 
-        <div class="user-details-container">
-            <!--                Left bar include the user profile picture, bio and the edit user and delete user buttons.-->
+        <div class="button-container">
+            <button id="btn-upload" class="buttons"><i class="fa fa-id-badge" aria-hidden="true"></i>Change Picture
+            </button>
+
+            <form action="<?php echo BASEURL . "/editProfile/upload" ?>" method="post" enctype="multipart/form-data"
+                  style="display:none" id="image-upload">
+                Upload file:
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <button type="submit" value="upload-pic" class="buttons"><i class="fa fa-upload" aria-hidden="true"></i>Upload
+                    Picture
+                </button>
+                <button id="btn-upload-cancel" class="buttons cancel"><i class="fa fa-ban" aria-hidden="true"></i>Cancel
+                </button>
+            </form>
+        </div>
+
+        <div class="details-container">
+            <!--                Left bar include the bio and miscellaneous-->
             <div class="left-bar">
-                <img <?php srcProfileIMG($data["userDetails"]["img"]); ?> alt="User image">
-
-                <button id="btn-upload"  class="buttons"><i class="fa fa-id-badge" aria-hidden="true"></i>Change Picture</button>
-
-                <form action="<?php echo BASEURL . "/editProfile/upload" ?>" method="post" enctype="multipart/form-data" style="display:none" id="image-upload">
-                    Upload file:
-                    <input type="file" name="fileToUpload" id="fileToUpload">
-                    <button type="submit" value="upload-pic" class="buttons"><i class="fa fa-upload" aria-hidden="true"></i>Upload Picture</button>
-                    <button id="btn-upload-cancel" class="buttons cancel"><i class="fa fa-ban" aria-hidden="true"></i>Cancel</button>
-                </form>
-
-                <div class="bio">
-                    <span><strong><?php echo $data["userDetails"]["first_name"] . " " . $data["userDetails"]["last_name"]; ?></strong></span>
-                    <span><?php echo $data["userDetails"]["user_type"] ?></span>
-                    <span><?php echo $data["userDetails"]["reg_no"] ?></span>
-                </div>
-            </div>
-
-            <!--    Right bar include small separate boxes of user details, assigned courses and badges earned-->
-            <div class="right-bar">
                 <form action="<?php echo BASEURL . "/editProfile/submit" ?>" method="POST">
                     <div class="pvt-details">
                         <span>User Details</span>
                         <span class="form-group">
                             <label for="fName">First Name :</label>
-                            <input type="text" id="fName" name="fName" value="<?php echo $data["userDetails"]["first_name"]; ?>" class="" disabled>
+                            <input type="text" id="fName" name="fName"
+                                   value="<?php echo $data["userDetails"]["first_name"]; ?>" class="" disabled>
                         </span>
                         <span class="form-group">
                             <label for="lName">Last Name :</label>
-                            <input type="text" id="lName" name="lName" value="<?php echo $data["userDetails"]["last_name"]; ?>" class="" disabled>
+                            <input type="text" id="lName" name="lName"
+                                   value="<?php echo $data["userDetails"]["last_name"]; ?>" class="" disabled>
                         </span>
                         <span class="form-group">
                             <label for="email">Email :</label>
-                            <input type="text" id="email" name="email" value="<?php echo $data["userDetails"]["email"]; ?>" class="" disabled>
+                            <input type="text" id="email" name="email"
+                                   value="<?php echo $data["userDetails"]["email"]; ?>" class="" disabled>
                         </span>
                         <span class="form-group">
-                            <label for="tele">Telephone No :</label>
-                            <input type="text" id="tele" name="tele" value="<?php echo $data["userDetails"]["tele"]; ?>" class="" disabled>
+                            <label for="tele">Tele. No :</label>
+                            <input type="text" id="tele" name="tele" value="<?php echo $data["userDetails"]["tele"]; ?>"
+                                   class="" disabled>
                         </span>
 
                         <div class="save-buttons">
                             <button id="btn-save" type="submit" value="Save" class="buttons" style="display: none">
                                 <i class="fa fa-floppy-o" aria-hidden="true"></i>Save
                             </button>
-                            <button id="btn-cancel" type="reset" value="Cancel" class="buttons cancel" style="display: none">
+                            <button id="btn-cancel" type="reset" value="Cancel" class="buttons cancel"
+                                    style="display: none">
                                 <i class="fa fa-ban" aria-hidden="true"></i>Cancel
                             </button>
                         </div>
@@ -90,6 +96,15 @@
                         </button>
                     </div>
                 </form>
+                <div class="miscel">
+                    <span>Miscellaneous</span>
+                    <span>Forum Posts</span>
+                    <span>Forum Discussions</span>
+                </div>
+            </div>
+
+            <!--    Right bar include small separate boxes of user details, assigned courses and badges earned-->
+            <div class="right-bar">
                 <div class="course-details">
                     <span>Course Details</span>
                     <?php
@@ -101,24 +116,21 @@
                     }
                     ?>
                 </div>
-                <div class="miscel">
-                    <span>Miscellaneous</span>
-                    <span>Forum Posts</span>
-                    <span>Forum Discussions</span>
-                </div>
             </div>
         </div>
+
     </div>
+</div>
 
-    <?php linkPhp("footer"); ?>
+<?php linkPhp("footer"); ?>
 
-    <?php linkPhp("notificationView"); ?>
+<?php linkPhp("notificationView"); ?>
 
-    <?php linkJS("lib/jquery-3.6.0.min"); ?>
+<?php linkJS("lib/jquery-3.6.0.min"); ?>
 
-    <?php linkJS("notification") ?>
+<?php linkJS("notification") ?>
 
-    <?php linkJS("editProfile"); ?>
+<?php linkJS("editProfile"); ?>
 
 </body>
 
