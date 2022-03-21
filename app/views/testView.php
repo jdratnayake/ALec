20cdca79cb1987354009261331d8de0bb1d60fee
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-03-21 16:07:47 +0530

    Add table to test view - statistics of quizzes

--- app/views/testView.php
+++ app/views/testView.php
@@ -19,40 +19,77 @@
 
 <body>
 
-    <?php linkPhp("navigationBarLecturer") ?>
+<?php linkPhp("navigationBarLecturer") ?>
 
-    <!--    breadcrumb-->
-    <ul class="breadcrumb">
-        <li><a href="http://localhost/ALec/lecturerDashboard/index">Home</a></li>
-        <li></li>
-    </ul>
+<!--    breadcrumb-->
+<ul class="breadcrumb" style="margin-bottom: 0">
+    <li><a href="http://localhost/ALec/lecturerDashboard/index">Home</a></li>
+    <li>Results</li>
+</ul>
 
-    <input type="hidden" id="quizSummary-label" value="<?php echo $data["quizSummary"]["label"]; ?>">
-    <input type="hidden" id="quizSummary-data" value="<?php echo $data["quizSummary"]["data"]; ?>">
+<input type="hidden" id="quizSummary-label" value="<?php echo $data["quizSummary"]["label"]; ?>">
+<input type="hidden" id="quizSummary-data" value="<?php echo $data["quizSummary"]["data"]; ?>">
 
-    <input type="hidden" id="quizQuestionSummary-label" value="<?php echo $data["quizQuestionSummary"]["label"]; ?>">
-    <input type="hidden" id="quizQuestionSummary-data" value="<?php echo $data["quizQuestionSummary"]["data"]; ?>">
+<input type="hidden" id="quizQuestionSummary-label" value="<?php echo $data["quizQuestionSummary"]["label"]; ?>">
+<input type="hidden" id="quizQuestionSummary-data" value="<?php echo $data["quizQuestionSummary"]["data"]; ?>">
 
-    <?php
 
-    ?>
+<div class="details-container">
 
-    <div class="details-container">
-        <header>Poll Statistics</header>
-
-        <canvas id="myChartQuiz" style="width:50%;max-width:1000px"></canvas>
-
-        <canvas id="myChartQuizQuestion" style="width:50%;max-width:1000px"></canvas>
-
+    <div class="col-1">
+        <ul style="list-style-type:none">
+            <li><a href="">Quiz 1</a></li>
+            <li><a href="">Quiz 2</a></li>
+            <li><a href="">Quiz 3</a></li>
+            <li><a href="">Quiz 4</a></li>
+            <li><a href="">Quiz 1</a></li>
+            <li><a href="">Quiz 2</a></li>
+        </ul>
     </div>
 
-    <?php linkPhp("footer"); ?>
+    <div class="col-2" style="">
+        <header>Poll Statistics</header>
+        <div class="chart-container">
+            <canvas class="chart" id="myChartQuiz"></canvas>
 
-    <?php linkJS("lib/jquery-3.6.0.min"); ?>
+            <canvas class="chart" id="myChartQuizQuestion"></canvas>
+        </div>
 
-    <?php linkJS("lib/chart.min"); ?>
+        <div class="table-container">
+            <table>
+                <thead>
+                <tr>
+                    <th>Index No.</th>
+                    <th>Name</th>
+                    <th>Total Marks</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>19000472</td>
+                    <td>Pansilu Gunaratne</td>
+                    <td>64.00%</td>
+                </tr>
+                <tr>
+                    <td>19001428</td>
+                    <td>Janitha Ratnayake</td>
+                    <td>98.00%</td>
+                </tr>
 
-    <?php linkJS("temp"); ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
+
+<?php linkPhp("footer"); ?>
+
+<?php linkJS("lib/jquery-3.6.0.min"); ?>
+
+<?php linkJS("lib/chart.min"); ?>
+
+<?php linkJS("temp"); ?>
 
 </body>
 
