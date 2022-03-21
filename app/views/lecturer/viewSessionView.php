094a535b8e5737a83255b54cef62ddae7842911a
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-03-22 02:06:08 +0530

    Update dots to trash in view session UI

--- app/views/lecturer/viewSessionView.php
+++ app/views/lecturer/viewSessionView.php
@@ -19,65 +19,67 @@
 </head>
 
 <body>
-    <input type="hidden" id="session-id" value="<?php echo $data["sessionData"]["session_id"]; ?>">
+<input type="hidden" id="session-id" value="<?php echo $data["sessionData"]["session_id"]; ?>">
 
-    <?php linkPhp("navigationBarLecturer"); ?>
+<?php linkPhp("navigationBarLecturer"); ?>
 
-    <!--    breadcrumb-->
-    <ul class="breadcrumb">
-        <li><a href="http://localhost/ALec/adminDashboard/index">Home</a></li>
-        <li><a href="http://localhost/ALec/displaySessionsList/index">Sessions</a></li>
-        <li>View Session</li>
-    </ul>
+<!--    breadcrumb-->
+<ul class="breadcrumb">
+    <li><a href="http://localhost/ALec/adminDashboard/index">Home</a></li>
+    <li><a href="http://localhost/ALec/displaySessionsList/index">Sessions</a></li>
+    <li>View Session</li>
+</ul>
 
-    <div class="details-content">
-        <div class="header-container">
-            <h2>
-                <?php echo $data["sessionData"]["course_name"]; ?>
-            </h2>
-            <header>
+<div class="details-content">
+    <div class="header-container">
+        <h2>
+            <?php echo $data["sessionData"]["course_name"]; ?>
+        </h2>
+        <header>
 
-                <i class="fa fa-pencil" aria-hidden="true" id="edit-name"></i>
+            <i class="fa fa-pencil" aria-hidden="true" id="edit-name"></i>
 
-                <?php echo $data["sessionData"]["session_name"]; ?>
+            <?php echo $data["sessionData"]["session_name"]; ?>
 
-                <?php
-                if ($data["sessionData"]["status"] == "T") {
-                    echo "<i class='fa fa-circle' id='active-status' aria-hidden='true' style='display: block'></i>";
-                } else
-                    echo "<i class='fa fa-circle' id='active-status' aria-hidden='true' style='display: none'></i>";
-                ?>
-
-            </header>
-
-            <?php
-
-            if ($data["sessionData"]["question_status"] == "T") {
-                $addStyle = "display: none";
-                $removeStyle = "display: block";
-            } else {
-                $addStyle = "display: block";
-                $removeStyle = "display: none";
-            }
-
-            ?>
-
-            <!--        Add to Course Page Button-->
-            <button type='button' value='Add to Course Page' class='export-btn' id='add-course-page-btn' style='<?php echo $addStyle; ?>'>
-                Add to Course Page
-            </button>
-
-            <!--        Remove from Course Page Button-->
-            <button type='button' value='Remove from Course Page' class='export-btn rmv-btn' id='rmv-course-page-btn' style='<?php echo $removeStyle; ?>'>
-                Remove from Course Page
-            </button>
-
-        </div>
-        <div class="button-container">
             <?php
             if ($data["sessionData"]["status"] == "T") {
-                echo
-                "
+                echo "<i class='fa fa-circle' id='active-status' aria-hidden='true' style='display: block'></i>";
+            } else
+                echo "<i class='fa fa-circle' id='active-status' aria-hidden='true' style='display: none'></i>";
+            ?>
+
+        </header>
+
+        <?php
+
+        if ($data["sessionData"]["question_status"] == "T") {
+            $addStyle = "display: none";
+            $removeStyle = "display: block";
+        } else {
+            $addStyle = "display: block";
+            $removeStyle = "display: none";
+        }
+
+        ?>
+
+        <!--        Add to Course Page Button-->
+        <button type='button' value='Add to Course Page' class='export-btn' id='add-course-page-btn'
+                style='<?php echo $addStyle; ?>'>
+            Add to Course Page
+        </button>
+
+        <!--        Remove from Course Page Button-->
+        <button type='button' value='Remove from Course Page' class='export-btn rmv-btn' id='rmv-course-page-btn'
+                style='<?php echo $removeStyle; ?>'>
+            Remove from Course Page
+        </button>
+
+    </div>
+    <div class="button-container">
+        <?php
+        if ($data["sessionData"]["status"] == "T") {
+            echo
+            "
                 <!--        End Session Button-->
                 <button type='button' value='End Session' class='main-btn' id='end-session' style='display: block' onclick='endFunction()'>
                     <i class='fa fa-stop' aria-hidden='true'></i>
@@ -89,9 +91,9 @@
                     Start Session
                 </button>
                 ";
-            } else {
-                echo
-                "
+        } else {
+            echo
+            "
                 <!--        End Session Button-->
                 <button type='button' value='End Session' class='main-btn' id='end-session' style='display: none' onclick='endFunction()'>
                     <i class='fa fa-stop' aria-hidden='true'></i>
@@ -103,109 +105,114 @@
                     Start Session
                 </button>
                 ";
-            }
-            ?>
-            <!--        Delete Session Button-->
+        }
+        ?>
+        <!--        Delete Session Button-->
 
-            <?php
-            $link = "'" . "viewSession/deleteSession/" . $data["sessionData"]["session_id"] . "'";
-            ?>
+        <?php
+        $link = "'" . "viewSession/deleteSession/" . $data["sessionData"]["session_id"] . "'";
+        ?>
 
-            <button type='button' value='Delete Session' class='main-btn dlt-btn' id='delete-session' style='display: block' <?php echo 'onclick="deleteFunction(' . $link . ')"'; ?>>
-                <i class='fa fa-trash' aria-hidden='true'></i>
-                Delete Session
-            </button>
+        <button type='button' value='Delete Session' class='main-btn dlt-btn' id='delete-session'
+                style='display: block' <?php echo 'onclick="deleteFunction(' . $link . ')"'; ?>>
+            <i class='fa fa-trash' aria-hidden='true'></i>
+            Delete Session
+        </button>
 
-        </div>
-        <div class="session-details-container">
-            <!-- <div class="sessions">
+    </div>
+    <div class="session-details-container">
+        <!-- <div class="sessions">
 <span class="session-label">Quick Questions</span>
 
 <button type="button" value="Add" class="add-btn"><i class="fa fa-plus" aria-hidden="true"></i>Add</button>
 <span class="session">
-    What's the best piece of advice you've ever been given?
-    <span>
-        <i class="fa fa-eye publish-status" aria-hidden="true"></i>
-        <i class="fa fa-eye-slash publish-status" aria-hidden="true" style="display: none"></i>
-    </span>
+What's the best piece of advice you've ever been given?
+<span>
+    <i class="fa fa-eye publish-status" aria-hidden="true"></i>
+    <i class="fa fa-eye-slash publish-status" aria-hidden="true" style="display: none"></i>
+</span>
 </span>
 <span class='session'>
-    What's the best piece of advice?
-    <span>
-        <i class='fa fa-eye publish-status' aria-hidden='true' style='display: none'></i>
-        <i class='fa fa-eye-slash publish-status' aria-hidden='true'></i>
-    </span>
+What's the best piece of advice?
+<span>
+    <i class='fa fa-eye publish-status' aria-hidden='true' style='display: none'></i>
+    <i class='fa fa-eye-slash publish-status' aria-hidden='true'></i>
+</span>
 </span>
 </div> -->
 
-            <div class="sessions">
-                <span class="session-label">Poll Questions</span>
-                <!--        Add Quick Question Button-->
-                <button type="button" value="Add" class="add-btn" onclick="location.href='<?php echo BASEURL . "/createPoll/index/{$data["sessionData"]["session_id"]}" ?>'">
-                    <i class="fa fa-plus" aria-hidden="true"></i>Add
-                </button>
+        <div class="sessions">
+            <span class="session-label">Poll Questions</span>
+            <!--        Add Quick Question Button-->
+            <button type="button" value="Add" class="add-btn"
+                    onclick="location.href='<?php echo BASEURL . "/createPoll/index/{$data["sessionData"]["session_id"]}" ?>'">
+                <i class="fa fa-plus" aria-hidden="true"></i>Add
+            </button>
 
-                <?php
-                if (mysqli_num_rows($data["questionDetails"]) == 0) {
-                    echo
-                    "
+            <?php
+            if (mysqli_num_rows($data["questionDetails"]) == 0) {
+                echo
+                "
                     <span class='session inactive'>No questions to show</span>
                     ";
-                }
+            }
 
-                while ($row = mysqli_fetch_assoc($data["questionDetails"])) {
-                    $questionLink = "'" . "viewSession/deleteSessionQuestion/" . $data["sessionData"]["session_id"] . "/" . $row['question_no'] . "'";
+            while ($row = mysqli_fetch_assoc($data["questionDetails"])) {
+                $questionLink = "'" . "viewSession/deleteSessionQuestion/" . $data["sessionData"]["session_id"] . "/" . $row['question_no'] . "'";
 
-                    echo
+                echo
                     "
                     <span class='session'>
-                    <i class='fa fa-ellipsis-v' aria-hidden='true'></i>
+                    <i class='fa fa-trash' aria-hidden='true' onclick=\"deleteFunction($questionLink)\"></i>
+                    
+                   <!-- <i class='fa fa-ellipsis-v' aria-hidden='true'></i> 
                     <span class='option-selection'>
                         <div id='option'>
                             <span onclick=\"deleteFunction($questionLink)\">Delete</span>
                         </div>
-                    </span>
+                    </span> -->
+                    
                     <span class='question'" . " onclick=\"window.location='http://localhost/ALec/showPollAnswer/index/{$row['question_no']}'\"" . ">{$row['question']}</span>
                         <span class='status-symbols'>
                         <input type='hidden' value='{$row['status']}'>
                         <input type='hidden' value='{$row['question_no']}'>
                     ";
 
-                    if ($row["status"] == "T") {
-                        echo
-                        "
+                if ($row["status"] == "T") {
+                    echo
+                    "
                         <i class='fa fa-eye publish-status' aria-hidden='true'></i>
                         <i class='fa fa-eye-slash publish-status' aria-hidden='true' style='display: none'></i>
                         ";
-                    } else {
-                        echo
-                        "
+                } else {
+                    echo
+                    "
                         <i class='fa fa-eye publish-status' aria-hidden='true' style='display: none'></i>
                         <i class='fa fa-eye-slash publish-status' aria-hidden='true'></i>
                         ";
-                    }
+                }
 
-                    echo
-                    "
+                echo
+                "
                         </span>
                     </span>
                     ";
-                }
-                ?>
+            }
+            ?>
 
-            </div>
-            <div class="sessions">
+        </div>
+        <div class="sessions">
 
-                <?php
+            <?php
 
-                $className = "style='display:none'";
+            $className = "style='display:none'";
 
-                if (mysqli_num_rows($data["forumQuestionDetails"]) == 0) {
-                    $className = "";
-                }
+            if (mysqli_num_rows($data["forumQuestionDetails"]) == 0) {
+                $className = "";
+            }
 
 
-                echo
+            echo
                 "
                     <span class='session-label link-label'>
                         <a href='" . BASEURL . "/viewSession/sessionForum/{$data['sessionData']['session_id']}'>
@@ -217,9 +224,9 @@
                 ";
 
 
-                while ($row = mysqli_fetch_assoc($data["forumQuestionDetails"])) {
-                    echo
-                    "
+            while ($row = mysqli_fetch_assoc($data["forumQuestionDetails"])) {
+                echo
+                "
                     <span class='session'>
                         {$row['question']}
                         <span class='vote'>
@@ -234,79 +241,82 @@
                         </span>
                     </span>
                     ";
-                }
-                ?>
+            }
+            ?>
 
-            </div>
         </div>
     </div>
+</div>
 
-    <!-- <span class='session inactive'>No questions to show</span> -->
-    <!-- <span class="session">
-    No questions to show
-    <span class="vote">
-        <div class='col'>
-            <i class='fa fa-check-circle check-resolved' aria-hidden='true'></i>
-            <span class='resolved-label'>Resolved</span>
-        </div>
-        <div class='col'>
-            <i class='fa fa-thumbs-o-up vote-highlight' aria-hidden='true'></i>
-            <span class='votes-count'>2</span>
-        </div>
-    </span>
+<!-- <span class='session inactive'>No questions to show</span> -->
+<!-- <span class="session">
+No questions to show
+<span class="vote">
+    <div class='col'>
+        <i class='fa fa-check-circle check-resolved' aria-hidden='true'></i>
+        <span class='resolved-label'>Resolved</span>
+    </div>
+    <div class='col'>
+        <i class='fa fa-thumbs-o-up vote-highlight' aria-hidden='true'></i>
+        <span class='votes-count'>2</span>
+    </div>
+</span>
 </span> -->
 
-    <!-- The Modal -->
-    <div id="edit-name-model" class="modal">
+<!-- The Modal -->
+<div id="edit-name-model" class="modal">
 
-        <!-- Modal content -->
-        <div class="modal-content">
-            <div class="modal-header">
-                <span class="close">&times;</span>
-                <h2>Rename Session</h2>
-            </div>
+    <!-- Modal content -->
+    <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">&times;</span>
+            <h2>Rename Session</h2>
+        </div>
 
-            <div class="modal-body">
-                <form action="<?php echo BASEURL . '/viewSession/updateSessionName/' . $data["sessionData"]["session_id"]; ?>" method="post" id="renameForm">
-                    <div class="date-group">
-                        <label for="sessionName">Session Name :</label>
+        <div class="modal-body">
+            <form action="<?php echo BASEURL . '/viewSession/updateSessionName/' . $data["sessionData"]["session_id"]; ?>"
+                  method="post" id="renameForm">
+                <div class="date-group">
+                    <label for="sessionName">Session Name :</label>
 
-                        <div class="right-side">
-                            <input type="text" id="sessionName" name="sessionName" value="<?php echo $data["sessionData"]["session_name"]; ?>" class="name-input" onfocusout="sessionNameValidityCheck()">
-                            <div class="error"></div>
-                        </div>
+                    <div class="right-side">
+                        <input type="text" id="sessionName" name="sessionName"
+                               value="<?php echo $data["sessionData"]["session_name"]; ?>" class="name-input"
+                               onfocusout="sessionNameValidityCheck()">
+                        <div class="error"></div>
                     </div>
-                    <input type="submit" value="Save" name="submit" class="upload-btn" id="save-btn">
-                </form>
-            </div>
+                </div>
+                <input type="submit" value="Save" name="submit" class="upload-btn" id="save-btn">
+            </form>
         </div>
     </div>
+</div>
 
-    <?php linkPhp("footer"); ?>
+<?php linkPhp("footer"); ?>
 
-    <?php linkPhp("notificationView"); ?>
+<?php linkPhp("notificationView"); ?>
 
-    <?php linkJS("lib/jquery-3.6.0.min"); ?>
+<?php linkJS("lib/jquery-3.6.0.min"); ?>
 
-    <?php linkJS("notification") ?>
+<?php linkJS("notification") ?>
 
-    <?php linkJS("viewSession"); ?>
+<?php linkJS("viewSession"); ?>
 
-    <?php linkJS("viewSessionQuestion"); ?>
+<?php linkJS("viewSessionQuestion"); ?>
 
-    <?php linkJS("viewSessionQuestionDisable"); ?>
+<?php linkJS("viewSessionQuestionDisable"); ?>
 
-    <?php linkJS("viewSessionOptionSelection"); ?>
+<?php linkJS("viewSessionOptionSelection"); ?>
 
-    <?php linkJS("viewSessionViewQuestionsUpdate") ?>
+<?php linkJS("viewSessionViewQuestionsUpdate") ?>
 
-    <?php linkJS("viewSessionViewQuestionStatus") ?>
+<?php linkJS("viewSessionViewQuestionStatus") ?>
 
-    <?php linkJS("sessionRenameValidation"); ?>
+<?php linkJS("sessionRenameValidation"); ?>
 
-    <?php linkJS("sessionQuestionPosted"); ?>
+<?php linkJS("sessionQuestionPosted"); ?>
 
-    <?php linkJS("deleteMessage"); ?>
+<?php linkJS("deleteMessage"); ?>
 
 </body>
 
