2fd14d48a41bc8fd953d882a9d1af0ea7bd117af
Janitha Ratnayake <janithadevin@gmail.com>
2022-03-23 08:46:35 +0530

    userProfile bug fixed

--- public/js/userProfileModal.js
+++ public/js/userProfileModal.js
@@ -1,3 +1,22 @@
+//When the user clicks assign courses button, open the model
+document.getElementById("modal-btn").addEventListener("click", () => {
+    document.getElementById("course-selection-modal").style.display = "block";
+})
+
+//When the user clicks the close button, close it
+document.getElementById("close").addEventListener("click", () => {
+    document.getElementById("course-selection-modal").style.display = "none";
+})
+
+//When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", (event) => {
+    if (event.target.classList.contains("modal")) {
+        for (let i = 0; i < document.getElementsByClassName("modal").length; i++) {
+            document.getElementsByClassName("modal")[i].style.display = "none";
+        }
+    }
+});
+
 let deleteCourseConfirm;
 
 $(document).ready(function () {
