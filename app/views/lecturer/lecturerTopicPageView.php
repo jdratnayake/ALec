c5921501ab674c2f2de507355cc06a7aeb7ec701
Janitha Ratnayake <janithadevin@gmail.com>
2022-03-25 23:09:11 +0530

    edit tag name done

--- app/views/lecturer/lecturerTopicPageView.php
+++ app/views/lecturer/lecturerTopicPageView.php
@@ -56,7 +56,7 @@
                 </button>
                 <div class='content'>
                 <div class='button-set'>
-                    <button class='edit-btn' id='edit-name'><i class='fa fa-pencil' aria-hidden='true'></i>Edit topic</button>
+                    <button class='edit-btn' id='edit-name' onclick=\"openEditTopicModal({$row['topic_id']}, '{$row['topic_name']}')\"><i class='fa fa-pencil' aria-hidden='true'></i>Edit topic</button>
                     <button class='dlt-btn' onclick=\"deleteFunction($questionLink)\"><i class='fa fa-trash' aria-hidden='true'></i>Delete topic</button>
                 </div>
                 <p class='quiz-head'>Quizzes</p>
@@ -147,17 +147,18 @@
             <div class="modal-body">
                 <form action="" method="post" id="editTopic">
                     <div class="date-group">
-                        <label for="sessionName">Topic Name :</label>
+                        <label for="topicName">Topic Name :</label>
 
                         <div class="right-side">
-                            <input type="text" id="sessionName" name="sessionName" value="" class="name-input"">
-                            <div class="error"></div>
+                            <input type="text" id="topicName" name="topicName" value="" class="name-input"">
+                            <div class=" error">
                         </div>
                     </div>
-                    <input type="submit" value="Save" name="submit" class="upload-btn" id="save-btn">
-                </form>
             </div>
+            <input type="submit" value="Save" name="submit" class="upload-btn" id="save-btn">
+            </form>
         </div>
+    </div>
     </div>
 
 
@@ -174,6 +175,8 @@
     <?php linkJS("successMessage"); ?>
 
     <?php linkJS("deleteMessage"); ?>
+
+    <?php linkJS("topicRenameValidation"); ?>
 
 </body>
 
--- public/js/topic_page.js
+++ public/js/topic_page.js
@@ -22,17 +22,19 @@ let btn = document.getElementById("edit-name");
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
+function openEditTopicModal(topicId, topicName) {
     modal.style.display = "block";
+    document.getElementById('editTopic').action = `http://localhost/ALec/lecturerTopicPage/updateTopicName/${topicId}`;
+    document.getElementById('topicName').value = topicName;
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
